02a9298dbf8ae5d841fea6e3bfe7cc77f0fb3bea
iduardo.gomes <iduardo.gomes@308-D-2291259.goias.intra>
2022-06-28 17:08:57 -0300

    add model Import and ImportItem

---
+++ src/Gesfrota/Model/Sys/Import.php
@@ -0,0 +1,110 @@
+<?php
+namespace Gesfrota\Model\Sys;
+
+use Gesfrota\Model\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
+
+/**
+ * Importação
+ * 
+ * @Entity
+ * @Table(name="imports")
+ */
+class Import extends Entity {
+    
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $description;
+    
+    /**
+     * @Column(name="created_at", type="datetime")
+     * @var \DateTime
+     */
+    protected $createdAt;
+    
+    /**
+     * @Column(name="filename", type="string")
+     * @var string
+     */
+    protected $fileName;
+    
+    /**
+     * @Column(name="filesize", type="integer")
+     * @var integer
+     */
+    protected $fileSize;
+    
+    /**
+     * @OneToMany(targetEntity="ImportItem", mappedBy="import", cascade={"all"})
+     * @var ArrayCollection
+     */
+    protected $items;
+    
+    
+    public function __construct() {
+        parent::__construct();
+        $this->createdAt = new \DateTime();
+    }
+    /**
+     * @return string
+     */
+    public function getDescription() {
+        return $this->description;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileName() {
+        return $this->fileName;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getFileSize() {
+        return $this->fileSize;
+    }
+
+    /**
+     * @return array
+     */
+    public function getItems() {
+        return $this->items->toArray();
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setDescription($text) {
+        $this->description = $text;
+    }
+    
+    /**
+     * @return integer
+     */
+    public function getAmountItems() {
+        return $this->items->count();
+    }
+    
+    /**
+     * @return integer
+     */
+    public function getAmountImported() {
+        $criteria = Criteria::create();
+        $criteria->where(Criteria::expr()->neq('reference', null));
+        return $this->items->count($criteria);
+    }
+
+}
+?>
---
+++ src/Gesfrota/Model/Sys/ImportItem.php
@@ -0,0 +1,99 @@
+<?php
+namespace Gesfrota\Model\Sys;
+
+use Gesfrota\Model\Entity;
+use Gesfrota\Model\Domain\Agency;
+use Gesfrota\Model\Domain\FleetItem;
+
+/**
+ * @Entity
+ * @Table(name="import_items")
+ */
+class ImportItem extends Entity {
+    
+    /**
+	 * @Column(type="array")
+	 * @var array
+	 */
+    protected $data;
+    
+    /**
+     * @ManyToOne(targetEntity="Import", inversedBy="items")
+     * @JoinColumn(name="import_id", referencedColumnName="id")
+     * @var Import
+     */
+    protected $import;
+    
+     /**
+     * @ManyToOne(targetEntity="Agency")
+     * @JoinColumn(name="agency_id", referencedColumnName="id")
+     * @var Agency
+     */
+    protected $agency;
+    
+    /**
+     * @ManyToOne(targetEntity="FleetItem")
+     * @JoinColumn(name="fleet_item_id", referencedColumnName="id")
+     * @var FleetItem
+     */
+    protected $reference;
+    
+    /**
+     * @param array $data
+     */
+    public function __construct(array $data) {
+        parent::__construct();
+        $this->data = $data;
+    }
+    
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @return Import
+     */
+    public function getImport() {
+        return $this->import;
+    }
+
+    /**
+     * @return Agency
+     */
+    public function getAgency() {
+        return $this->agency;
+    }
+
+    /**
+     * @return FleetItem
+     */
+    public function getReference() {
+        return $this->reference;
+    }
+
+    /**
+     * @param Import $import
+     */
+    public function setImport(Import $import) {
+        $this->import = $import;
+    }
+
+    /**
+     * @param Agency $agency
+     */
+    public function setAgency(Agency $agency) {
+        $this->agency = $agency;
+    }
+
+    /**
+     * @param FleetItem $reference
+     */
+    public function setReference(FleetItem $reference)  {
+        $this->reference = $reference;
+    }
+    
+}
