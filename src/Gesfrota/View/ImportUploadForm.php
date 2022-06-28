5be5d534c9a8066c67fc9111d576ff10680e7d6d
iduardo.gomes <iduardo.gomes@308-D-2291259.goias.intra>
2022-06-28 17:41:05 -0300

    add view ImportUploadForm

---
+++ src/Gesfrota/View/ImportUploadForm.php
@@ -0,0 +1,82 @@
+<?php
+namespace Gesfrota\View;
+
+use Gesfrota\Model\Domain\Agency;
+use Gesfrota\View\Widget\AbstractForm;
+use PHPBootstrap\Validate\Pattern\Email;
+use PHPBootstrap\Validate\Pattern\Pattern;
+use PHPBootstrap\Validate\Required\Required;
+use PHPBootstrap\Widget\Action\Action;
+use PHPBootstrap\Widget\Form\Controls\Fieldset;
+use PHPBootstrap\Widget\Form\Controls\TextBox;
+use PHPBootstrap\Widget\Form\Controls\XFileBox;
+use PHPBootstrap\Widget\Form\Controls\Decorator\Mask;
+use PHPBootstrap\Widget\Nav\NavLink;
+use PHPBootstrap\Widget\Nav\TabPane;
+use PHPBootstrap\Widget\Nav\Tabbable;
+use Gesfrota\Model\Sys\Import;
+use PHPBootstrap\Validate\Pattern\Upload;
+
+class ImportUploadForm extends AbstractForm {
+	
+	/**
+	 * Construtor
+	 * 
+	 * @param Action $submit
+	 * @param Action $cancel
+	 */
+	public function __construct( Action $submit, Action $cancel ) {
+		$this->buildPanel('Sistema', 'Gerenciar Importações');
+		$form = $this->buildForm('import-upload-form');
+		
+		$fieldset = new Fieldset('Upload');
+		
+		$input = new TextBox('desc');
+		$input->setSpan(7);
+		$input->setRequired(new Required(null, 'Por favor, preencha esse campo'));
+		$input->setValue('Importação feita na ' . ucfirst(utf8_encode(strftime('%A, %d %B %G %T', strtotime('now')))));
+		$form->buildField('Descrição', $input, null, $fieldset);
+		
+		$input = new XFileBox('file');
+		$input->setPlaceholder('Escolha um arquivo .csv');
+		$input->setRequired(new Required(null, 'Por favor, preencha esse campo'));
+		$input->setPattern(new Upload(['text/csv' => 'csv'], 'Informe um arquivo .csv'));
+		$input->setSpan(3);
+		$form->buildField(null, $input, null, $fieldset);
+		
+		$tab = new Tabbable('import-tabs');
+		$tab->setPlacement(Tabbable::Left);
+		$tab->addItem(new NavLink('Upload'), null, new TabPane($fieldset));
+		$tab->addItem(new NavLink('Pré-processamento'));
+		$tab->addItem(new NavLink('Transformação'));
+		
+		$form->append($tab);
+
+		$form->buildButton('submit', 'Executar', $submit);
+		$form->buildButton('cancel', 'Cancelar', $cancel);
+	}
+	
+	/**
+	 * @see AbstractForm::extract()
+	 */
+	public function extract( Import $object ) {
+		$data['desc'] = $object->getDescription();
+		$this->component->setData($data);
+	}
+
+	/**
+	 * @see AbstractForm::hydrate()
+	 */
+	public function hydrate( Import $object ) {
+		$data = $this->component->getData();
+		$object->setDescription($data['name']);
+		$object->setFile($data['nif']);
+		$object->setAcronym($data['acronym']);
+		$object->setContact($data['contact']);
+		$object->setEmail($data['email']);
+		$object->setPhone($data['phone']);
+		$object->setActive($data['active']);
+	}
+
+}
+?>
