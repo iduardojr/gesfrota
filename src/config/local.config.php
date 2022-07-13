510523bb8488488eed56d7c69ec1cda61adb3c8f
iduardo.gomes <iduardo.gomes@308-D-2291259.goias.intra>
2022-07-13 14:58:10 -0300

    update config local

---
+++ src/config/local.config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'doctrine' => [
+		'connection' => [
+			'driver' => 'pdo_mysql',
+			'host' => 'localhost',
+			'port' => '3306',
+			'user' => 'root',
+			'password' => 'root',
+			'dbname' => 'gesfrota']
+	]
+];
+?>
